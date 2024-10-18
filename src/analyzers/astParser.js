dbcbff92704cccd97d8bdbb085145243eb560a09
genGit963 <bogati.mahesh.299.792.458@gmail.com>
2024-10-19 01:22:47 +0545

    work on it simple line testing

--- src/analyzers/astParser.js
+++ src/analyzers/astParser.js
@@ -1,1 +1,18 @@
+const parser = require("@babel/parser");
 
+function parseCodeToAST(code) {
+  return parser.parse(code, {
+    sourceType: "module",
+    plugins: ["jsx"],
+  });
+}
+
+function traverseAST(ast, visitor) {
+  const traverse = require("@babel/traverse").default;
+  traverse(ast, visitor);
+}
+
+// const result = parseCodeToAST(``);
+// console.log(result);
+
+module.exports = { parseCodeToAST, traverseAST };
